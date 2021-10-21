bab3986b3271c51dcbf288e7a0b5b031caa288d0
you lee <letsgosmoke@gmail.com>
2021-10-21 22:14:03 +0800

    Правда или действие (часть 1)

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
-import {getRandomInt, checkCommentLength, GenerateComments, CreateDescription} from './utils/functions.js';
+import {GenerateComments, CreateDescription} from './utils/mock.js';
 import {showAllPictures} from './modules/show-photo.js';
+//import {showComments} from './modules/show-big-pick.js';
+import './modules/form-handler.js';
 
 //for linter
-checkCommentLength('some text for test', 10);
-getRandomInt();
+//checkCommentLength('some text for test', 10);
 GenerateComments();
 //console.log(CreateDescription());
 
+
 //insert pictures
 showAllPictures(CreateDescription());
+
+//showBigPicture ();
---
+++ js/modules/show-big-pick.js
@@ -0,0 +1,13 @@
+//export const showBigPicture = () = {};
+/*
+export const showComments = () => {
+  let socialComment = document.querySelector('.social__comment');
+  let allComents = document.createDocumentFragment();
+  let socialComments = document.querySelector('.social__comments');
+  socialComments.innerHTML = '';
+
+  console.log(socialComment);
+
+}
+
+*/
